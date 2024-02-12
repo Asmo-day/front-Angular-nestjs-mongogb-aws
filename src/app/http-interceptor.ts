e3295bd46e9ee08679200bf0ade470577adbeb4f
asmoday <delfrick2@gmail.com>
2024-02-12 12:03:54 +0100

    complete the interceptor feature

--- src/app/http-interceptor.ts
+++ src/app/http-interceptor.ts
@@ -1,26 +1,15 @@
-import { HttpClient, HttpHeaders, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { map, switchMap, tap } from 'rxjs';
 import { TokenService } from './token.service';
+import { switchMap } from 'rxjs';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
 
-  console.log(req);
-  
-
-  let headerWithToken: any;
   const tokenService = inject(TokenService)
-  tokenService.getToken().subscribe(response => {
-    console.log(response);
-    
-    // switchMap(() => token = response.token)
-    headerWithToken = req.clone({
+
+  return tokenService.getToken().pipe(
+    switchMap(response => next(req.clone({
       headers: req.headers.set('Authorization', `Bearer ${response.token}`)
-    })
-  });
-  // console.log('################### ' + token);
-  
-
-
-  return next(headerWithToken);
+    })))
+  );
 };
