e3917bc00356255a6e81b40d14122b41f1f0e55a
Eric DORME <eric.dorme@niji.fr>
2024-02-22 11:38:37 +0100

    wip user management feature

---
+++ src/app/shared/dialog-box/logout-delete-dialog/logout-delete.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout-delete.component.html',
+  styleUrl: './logout-delete.component.scss'
+})
+export class LogoutDeleteComponent {
+
+  public title: string = '';
+
+  constructor(
+    private dialogRef: MatDialogRef<LogoutDeleteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { this.title = data.title }
+
+  logout() {
+    this.dialogRef.close(true);
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
+
+}
