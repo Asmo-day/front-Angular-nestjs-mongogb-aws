fa6c921d369f66525c9819b0a0f104271211f34c
Eric DORME <eric.dorme@niji.fr>
2024-02-23 12:03:58 +0100

    add user createDate and lastConnectionDate and wip user photo editor feature

---
+++ src/app/shared/dialog-box/edit-user-icon-dialog/edit-user-icon-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-user-icon-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-user-icon-dialog.component.html',
+  styleUrl: './edit-user-icon-dialog.component.scss'
+})
+export class EditUserIconDialogComponent {
+
+}
