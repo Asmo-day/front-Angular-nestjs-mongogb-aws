1e591a0365b57a9db2f16bdc2e8598f7d15bdeba
asmoday <delfrick2@gmail.com>
2024-03-11 11:37:46 +0100

    change ec2 ip

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
 
-    BASE_URL: "http://35.163.180.116:3000/",
+    BASE_URL: "http://34.210.239.68:3000/",
     API_KEY: '85e85d0a6efdf600eb89fc401a023c8d5dec650cf73a96549ffc0f9f0b00b312',
     CRYPTO_KEY: '2c266c5c5c2b700c5300991d62bd2e6f07a473b35361fcfe72868af85c0f7957'
 };
